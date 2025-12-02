39ac6e5a609b1a2ad5f9c374b945d67ff2c92e0f
Swapnilgithub23 <144453913+Swapnilgithub23@users.noreply.github.com>
2025-12-02 19:48:01 +0530

    Update gates.groovy

--- vars/gates.groovy
+++ vars/gates.groovy
@@ -1,5 +1,5 @@
 def call(){
-   timeout(time: 2, unit: "MINUTES"){
-      waitForQualityGates abortPipeline: false
+    timeout(time: 2, unit: "MINUTES"){
+       waitForQualityGates abortPipeline: false
 }
 }
