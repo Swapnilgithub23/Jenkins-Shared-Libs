b1436033e845021022015e3b0477ef0f48ed1242
Swapnilgithub23 <144453913+Swapnilgithub23@users.noreply.github.com>
2025-12-27 16:09:54 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,11 @@
-def call(){
-    sh "docker build -t online-shop-app ."
+def call(Map config = [:]){
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def dockerfile = config.dockerfile ?: 'dockerfile'
+    def context = config.context ?: '.'
+    
+    echo "Building Docker Image: ${imageName}:${imageTag} using ${dockerfile}"
+    sh """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+        """
 }
