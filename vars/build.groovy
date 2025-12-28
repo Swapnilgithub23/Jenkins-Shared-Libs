d6daf5e343367c9cad898e32998c3e12dc52ec40
Swapnilgithub23 <144453913+Swapnilgithub23@users.noreply.github.com>
2025-12-28 14:41:35 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,11 +1,12 @@
-def call(Map config = [:]){
+def call(Map config = [:]) {
     def imageName = config.imageName ?: error("Image name is required")
     def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'dockerfile'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
     def context = config.context ?: '.'
     
-    echo "Building Docker Image: ${imageName}:${imageTag} using ${dockerfile}"
+    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
+    
     sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest f ${dockerfile} ${context}
-        """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+    """
 }
