c35c0eab26888c14bfd0b38c34c5990dad314e84
Swapnilgithub23 <144453913+Swapnilgithub23@users.noreply.github.com>
2025-12-28 17:14:47 +0530

    Update trivy_image.groovy

--- vars/trivy_image.groovy
+++ vars/trivy_image.groovy
@@ -1,4 +1,4 @@
 def call() {
-  sh "trivy image --format json tws-app:latest > trivy-report1.json"
-  sh "trivy image --format json tws-app-migration:latest > trivy-report2.json"
+  sh "trivy image --format json swapnildocker23/easyshop-app > trivy-report1.json"
+  sh "trivy image --format json swapnildocker23/easyshop-migration > trivy-report2.json"
 }
